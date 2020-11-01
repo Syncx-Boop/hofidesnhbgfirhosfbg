507e74aec820a3a94bfebde73903b435cf886b97
Syncx-Boop <73795840+Syncx-Boop@users.noreply.github.com>
2020-11-01 20:22:02 +0100

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,31 @@
+import discord
+import asyncio
+import random 
+
+opt = ["schere","papier","stein"]
+
+client = discord.Client()
+@client.event
+async def on_message(message):
+    if message.content.lower() in opt:
+        oopt = random.choice(opt)
+        if oopt == message.content.lower():
+            await message.channel.send("Gleichstand")
+        elif oopt == "schere" and message.content.lower() == "papier":
+            await message.channel.send ("Ich habe mit schere gewonnen")     
+        elif oopt == "papier" and message.content.lower() == "stein":
+            await message.channel.send ("Ich habe mit papier gewonnen")     
+        elif oopt == "stein" and message.content.lower() == "schere":
+            await message.channel.send ("Ich habe mit stein gewonnen")
+        elif oopt == "schere" and message.content.lower() == "stein":
+            await message.channel.send ("Ich habe mit schere verloren")     
+        elif oopt == "papier" and message.content.lower() == "schere":
+            await message.channel.send ("Ich habe mit papier verloren")     
+        elif oopt == "stein" and message.content.lower() == "papier":
+            await message.channel.send ("Ich habe mit stein verloren")     
+
+
+
+
+token = "NzcyNTE3ODU5NTIzNjkwNTE3.X571Ug.hylyrNwUy7suE9-4rqSu2byYOQk"
+client.run(token)
